09ab39bbbb2c18a86b6a23e813a0cdf069f29ed7
perveshossen <62029218+perveshossen@users.noreply.github.com>
2020-03-10 23:55:30 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+
+$('.portfolio_img').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    nextArrow: '<i class="fa fa-arrow-right right_arrow" aria-hidden="true"></i>',
+    prevArrow: '<i class="fa fa-arrow-left left_arrow" aria-hidden="true"></i>',
+});
+
+$('.client_slide').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+});
+
+
+jQuery(document).ready(function ($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+});
