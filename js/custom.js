a028af5af9deddd4f9b71367733b134981dd7577
perveshossen <62029218+perveshossen@users.noreply.github.com>
2020-03-11 23:38:41 +0600

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,27 @@
+var html_body = $('html, body');
+    $('nav a').on('click', function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                html_body.animate({
+                    scrollTop: target.offset().top - 0
+                }, 1500, );
+                return false;
+            }
+        }
+    });
+
+$(window).scroll(function () {
+    var scroll = $(this).scrollTop();
+    if (scroll > 1) {
+        $(".nav_fixed").addClass('fixed');
+    } else {
+        $(".nav_fixed").removeClass('fixed');
+    }
+});
+
+
 
 $('.portfolio_img').slick({
     slidesToShow: 3,
